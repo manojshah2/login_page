368da197028b3d91e98931e1ec55d4b615864494
Manoj Shah <manoj.shh1@gmail.com>
2020-10-10 14:57:05 +0530

    search progressed.

--- results.php
+++ results.php
@@ -23,6 +23,22 @@ is_login($root);
 
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <style>
+    .profile {
+      background-color: white;
+    }
+    .profile_txt {
+      
+      font-size: 14px;
+      font-weight: 300;
+      color: #353e4f;
+    }
+    .about_txt {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  </style>
 
 </head>
 
@@ -56,12 +72,45 @@ is_login($root);
           
           <?php
           error_reporting(E_ERROR | E_PARSE);
+
+
           $sid=$_REQUEST['searchid'];
+          $page=0;          
+          $resultsperpage = 20;
+          if (isset($_REQUEST['page'])){
+            $page = $_REQUEST['page'];
+          }
+
+          $offset = ($page * $resultsperpage);
+          
+
+          function addSearchParam($search_condition, $column, $db_header ) {
+            if (strlen($column)>0){
+              $data_split = explode(",",$column);
+              $cond = "";
+              foreach($data_split as $key => $value) {  
+                $cond = $cond." or `".$db_header."`='".$value."'";
+              }
+              $cond = substr($cond,3);
+              if(strlen($search_condition)>0){
+                $search_condition = $search_condition." and (".$cond.") ";
+              }else {
+                $search_condition = $search_condition." (".$cond.") ";
+              }
+            }
+            return $search_condition;
+          }
+
             if(isset($_REQUEST['searchid'])){
               $result =$mysqli->query("select SEARCH from tblsearch where ID=".$_REQUEST['searchid']);
-              $data='';
+              $data=array();
               while($fetchdata=$result->fetch_array()){
                   $data=json_decode($fetchdata[0],true);
+              }
+
+              if (count($data)<1){
+                echo "Search Not Found. Please goto Search Profile and Perform Search Again.";
+                exit();
               }
 
               $search_condition="";
@@ -70,15 +119,64 @@ is_login($root);
               if(strlen($fromage)>0){
                   $search_condition = $search_condition." TIMESTAMPDIFF(YEAR,DOB,current_date())>=".$fromage." and TIMESTAMPDIFF(YEAR,DOB,current_date())<=".$toage; 
               }
-              $final_query="select * from tblprofiles where ".$search_condition;
-              //print_r($final_query);
-              $result =$mysqli->query($final_query);
+              //print_r($data);
+              $search_condition = addSearchParam($search_condition,$data["religion"],"RELIGION");
+              $search_condition = addSearchParam($search_condition,$data["caste"],"CASTE");
+              $search_condition = addSearchParam($search_condition,$data["country"],"COUNTRY OF RESIDENCE");
+              $search_condition = addSearchParam($search_condition,$data["state"],"STATE OF RESIDENCE");
+              $search_condition = addSearchParam($search_condition,$data["open_for_divorce"],"Open For Divorcee");
+              $search_condition = addSearchParam($search_condition,$data["open_for_outside_india"],"Open For Outside India");
+              $search_condition = addSearchParam($search_condition,$data["marital_status"],"MARITAL STATUS");
+              $search_condition = addSearchParam($search_condition,$data["manglik"],"MANGLIK");
+              $search_condition = addSearchParam($search_condition,$data["believes_in_horoscope"],"BELIEVES IN HOROSCOPE ");
+              $search_condition = addSearchParam($search_condition,$data["occupation"],"Occupation");
+              $search_condition = addSearchParam($search_condition,$data["from_premium_institute"],"From Premium Institute");
+              $search_condition = addSearchParam($search_condition,$data["residential_type"],"Residential Type");
+              $search_condition = addSearchParam($search_condition,$data["affluence_level"],"AFFLUENCE LEVEL");
+              $search_condition = addSearchParam($search_condition,$data["family_type"],"FAMILY TYPE");
+              $search_condition = addSearchParam($search_condition,$data["willing_to_stay"],"Willing To Stay");
+              $search_condition = addSearchParam($search_condition,$data["food_habits"],"FOOD HABITS");
+              $search_condition = addSearchParam($search_condition,$data["drink"],"DRINK");
+              $search_condition = addSearchParam($search_condition,$data["smoke"],"SMOKE");
+              $search_condition = addSearchParam($search_condition,$data["looks"],"LOOKS");
+              $search_condition = addSearchParam($search_condition,$data["body_type"],"BODY TYPE");
+              $search_condition = addSearchParam($search_condition,$data["communication"],"Communication");
+              $search_condition = addSearchParam($search_condition,$data["complexion"],"COMPLEXION");
+              
+              //print_r($search_condition);
+
+              if (strlen($search_condition)>0) {
+                
+                $final_query="select * from tblprofiles where ".$search_condition." LIMIT ".$resultsperpage." OFFSET ".$offset;
+                $count_query="select count(*) from tblprofiles where ".$search_condition;
+                //print_r($final_query);
+                $count_result = $mysqli->query($count_query);
+                $count = $count_result->fetch_array()[0];                
+                $result =$mysqli->query($final_query);
+              }
+              
+             
+              $totalPages = ceil($count / $resultsperpage);
+
             }
             
 
           ?>
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800"><?php echo mysqli_num_rows($result); ?> Results Found</h1>            
+            <h1 class="h3 mb-0 text-gray-800"><?php echo $count; ?> Matches Found</h1>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="row">                  
+                    <?php if ($page>0){ ?>
+                      <a class="btn btn-primary mr-4" href="results.php?searchid=<?php echo $sid ?>&page=<?php echo $page-1 ?>">Prev</a>
+                    <?php }; ?>
+                  
+                    <?php if ($page<$totalPages-1){ ?>
+                      <a class="btn btn-primary" href="results.php?searchid=<?php echo $sid ?>&page=<?php echo $page+1 ?>">Next</a>
+                    <?php }; ?>
+                </div>
+              </div>
+            </div>
           </div>
             
           
@@ -86,24 +184,26 @@ is_login($root);
             <div id='results' style='display:block;width:65%;'>
               <?php while($profile=$result->fetch_array()): ?>
               <div id='profile' class="pb-3">
-                <div class="row">
+                <div class="row profile">
                   <div class="col-md-4">
                     <?php
                       $profile_images = $mysqli->query('select `IMG PATH` from tblimages where PID='.$profile['ID']);
                       $images=$profile_images->fetch_array();
 
-                      if(strlen($images)==0){
-                        $images[0]="Default";
+                      if(count($images)==0){
+                        $images[0]="images/nophoto.png";
                       };
                     ?>
                     
                     <img src="<?php echo $images[0]; ?>" height="220px" width="220px">
                   </div>
-                  <div class="col-md-8">
+                  <div class="col-md-6 profile_txt">
                     <div class="row">
+                      <div class="pt-2">
                       <?php echo $profile['FIRST NAME']; ?> <?php echo $profile['LAST NAME']; ?>
+                      </div>
                     </div>
-                    <hr>
+                    <hr class="mt-2">
                     <div class="row">
                       <div class="col-md-5">
                         <div class="mb-0"><?php echo $profile['HEIGHT']; ?></div>
@@ -118,6 +218,11 @@ is_login($root);
                         <div class="mb-0"><?php echo $profile['MARITAL STATUS']; ?></div>
                       </div>
                       <div class="col-md-2"></div>
+                    </div>
+                    <div class="row pt-2">
+                      <div class="about_txt">
+                        <?php echo $profile['ABOUT'] ?>
+                      </div>
                     </div>
                   </div>
                 </div>
