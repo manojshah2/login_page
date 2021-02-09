63f54c313db3c061005a21a978a00a8474090453
Manoj Shah <manoj.shh1@gmail.com>
2021-02-09 19:13:18 +0530

    changesl

--- test/send.php
+++ test/send.php
@@ -5,22 +5,24 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Exchange\AMQPExchangeType;
 
-$exchange = 'router';
-$queue = 'msgs';
+$exchange = 'inTopic';
+$queue = 'mqtt-subscription-esp8266 clientqos0';
 
 $connection = new AMQPStreamConnection("52.91.158.17", 5672, "efqgdzpx", "uiiphqstv4wfz7kM4tx2vsBGBpuScnz8", "efqgdzpx");
 
 $channel = $connection->channel();
-$channel->queue_declare($queue, false, true, false, false);
-$channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
+//$channel->queue_declare($queue, false, false, false, false);
+$channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, false, false);
 
 $channel->queue_bind($queue, $exchange);
 
-$messageBody = "test";
-$message = new AMQPMessage($messageBody, array('content_type' => 'text/plain', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+$messageBody = $_REQUEST['ir'];
+$message = new AMQPMessage($messageBody, array('content_type' => 'text/plain'));
 $channel->basic_publish($message, $exchange);
 
 $channel->close();
 $connection->close();
+
+echo "Done";
 
 ?>
